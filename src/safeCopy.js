354f2c3a2809426277100ad1e0e9124a74f04feb
Michael Federczuk <federczuk.michael@protonmail.com>
2020-03-14 14:14:12 +0100

    Update copyright year

--- src/safeCopy.js
+++ src/safeCopy.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 6 */
 /*
  * A set of utility functions that recursively operate on objects.
- * Copyright (C) 2019 Michael Federczuk
+ * Copyright (C) 2020 Michael Federczuk
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
