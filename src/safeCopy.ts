026aa4fb955341d5f34689660a843dd53c81a9c6
Michael Federczuk <federczuk.michael@protonmail.com>
2020-03-14 21:24:27 +0100

    Add more overload signatures of safeCopy

--- src/safeCopy.ts
+++ src/safeCopy.ts
@@ -20,6 +20,26 @@ import deepCopy from "./deepCopy";
 import deepFreeze from "./deepFreeze";
 
 /**
+ * Creates a safe copy of **arr** by creating a deep frozen copy of it.
+ *
+ * @param arr
+ *        The array to create a safe copy of.
+ *
+ * @returns A safe copy of **arr**.
+ */
+function safeCopy<T>(arr: T[][]): readonly (readonly Readonly<T>[])[];
+
+/**
+ * Creates a safe copy of **arr** by creating a deep frozen copy of it.
+ *
+ * @param arr
+ *        The array to create a safe copy of.
+ *
+ * @returns A safe copy of **arr**.
+ */
+function safeCopy<T>(arr: T[]): readonly Readonly<T>[];
+
+/**
  * Creates a safe copy of **obj** by creating a deep frozen copy of it.
  *
  * @param obj
@@ -27,6 +47,10 @@ import deepFreeze from "./deepFreeze";
  *
  * @returns A safe copy of **obj**.
  */
-export default function safeCopy<T>(obj: T): T {
+function safeCopy<T>(obj: T): Readonly<T>;
+
+function safeCopy<T>(obj: (T[][] | T[] | T)): (readonly (readonly Readonly<T>[])[] | readonly Readonly<T>[] | Readonly<T>) {
 	return deepFreeze(deepCopy(obj));
 }
+
+export default safeCopy;
