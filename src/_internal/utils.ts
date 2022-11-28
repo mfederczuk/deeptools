dcd665e082457b70c16a9ec877a2239d305d3b42
Michael Federczuk <federczuk.michael@protonmail.com>
2022-11-28 18:56:30 +0100

    Add `src/types.ts` and `src/_internal/utils.ts`

---
+++ src/_internal/utils.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022 Michael Federczuk
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { GenericKey } from "../types";
+
+export type NonEmptyArray<T> = ([T, ...T[]] | [...T[], T]);
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function canValueHaveProperties(value: unknown): value is NonNullable<(object | Function)> {
+	return (((typeof value === "object") && (value !== null)) ||
+	        (typeof value === "function"));
+}
+
+export function getPropertyKeys(obj: NonNullable<unknown>): GenericKey[] {
+	return [
+		...(Object.getOwnPropertyNames(obj)),
+		...(Object.getOwnPropertySymbols(obj)),
+	];
+}
